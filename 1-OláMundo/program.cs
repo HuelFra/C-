e4dacce187f4c1eb08a473b9f4101d14dbee42b8
Huel <39540176+HuelFra@users.noreply.github.com>
2022-11-07 20:55:14 -0300

    Add files via upload

---
+++ 1-OláMundo/program.cs
@@ -0,0 +1,11 @@
+﻿using System; //O bloco using provê ao desenvolvedor a habilidade de se criar um bloco de código
+              //isolado dentro de um determinado programa.
+class Programa
+{
+    static void Main(string[] args) //o método Main() é o ponto onde o programa inicia sua execução.Ele deve estar dentro de uma classe ou struct e deve ser declarado com o modificador static.
+    {
+        Console.WriteLine("Olá mundo!");//WriteLine, grava a representação de texto dos objetos especificados
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();//Quando for lido algo que o usuário fez, ele fecha a execução. Ele lê e não tem mais nada para fazer.                      
+    }
+}
