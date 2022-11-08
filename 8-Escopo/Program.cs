510546e51028ef7d4c761d70032531e7ae081806
Huel <39540176+HuelFra@users.noreply.github.com>
2022-11-07 21:35:03 -0300

    Escopo.cs
    Utilizando condicionais dentro de um escopo.

---
+++ 8-Escopo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 8 - Condicionais2");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        bool acompanhado = quantidadePessoas > 1;
+
+        string textoadicional;
+
+        if (acompanhado == true) //Comparando os valores (==)
+        {
+            textoadicional = "João está acompanhado";//Atribuindo valores (=)
+        }
+        else
+        {
+            textoadicional = "João não está acompanhado";
+        }
+
+        if (idadeJoao >= 18 || acompanhado)
+        {
+            Console.WriteLine(textoadicional);
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar!");
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();
+    }
+}
