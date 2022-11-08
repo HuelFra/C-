606948eceace10b6af48f6e930107f126173413c
Huel <39540176+HuelFra@users.noreply.github.com>
2022-11-07 21:39:16 -0300

    CalculaPoupanca.cs

---
+++ 10-CalculaPoupança/Program.cs
@@ -0,0 +1,34 @@
+﻿//Resolução od Exercício proposto - Desafio Alíquota
+using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 11 - Calcula Poupança");
+        Console.WriteLine();
+
+        double investimento = 1000.0;
+
+
+        for (int mes = 1; mes <= 12; mes++)
+        {
+            investimento *= 1.005;
+            Console.WriteLine("No mês " + mes + " você tem R$ " + investimento);
+        }
+
+        /*
+         while(mes <=12)
+         {
+             investimento = investimento + investimento * 0.005;
+             Console.WriteLine("No mes " + mes + " você tem R$ " + investimento);
+
+             mes += 1;
+         }
+        */
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+     
+    }
+}
