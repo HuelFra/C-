1dd09997f46e87756268f736cc31e52a188a303f
Huel <39540176+HuelFra@users.noreply.github.com>
2022-11-07 21:32:48 -0300

    Condicionais2
    Continuação de como utilizar if e else.

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 8 - Condicionais2");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        bool acompanhado = quantidadePessoas > 1;
+        bool grupo = true;
+
+        if (idadeJoao >= 18 || grupo)
+        {
+            Console.WriteLine("Pode Entrar!");
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar!");
+        }
+
+            Console.WriteLine("Tecle Enter para fechar...");
+            Console.ReadLine();
+    }
+}
