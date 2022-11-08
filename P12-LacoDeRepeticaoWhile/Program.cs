6bde5db6381c40d59c633128dc2e5c2c76a4861a
Huel <39540176+HuelFra@users.noreply.github.com>
2022-11-07 21:52:34 -0300

    Add files via upload

---
+++ P12-LacoDeRepeticaoWhile/Program.cs
@@ -0,0 +1,28 @@
+﻿//Resolução do Exercício proposto - LacoDeRepeticaoWhile
+using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o exercício proposto (Adaptado) - LacoDeRepeticaoWhile");
+        Console.WriteLine("Soma números inteiros de 1 a 18");
+        Console.WriteLine();
+
+        int Soma = 0;
+        int i = 0;
+
+        while(i < 18)
+        {
+            Soma++;
+            i++;
+            Console.WriteLine("O valor da Soma é:" + Soma + " e o valor de i é:" + i);
+        }
+        
+        Console.WriteLine();
+    
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+
+    }
+}
