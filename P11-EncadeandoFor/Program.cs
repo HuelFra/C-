a83307ec6c3fecc602839e3196e9e9f11dbb8abc
Huel <39540176+HuelFra@users.noreply.github.com>
2022-11-07 21:49:39 -0300

    Utilizando Break
    Aprendendo a utilizar o break.

---
+++ P11-EncadeandoFor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 13 - Encadeando For");
+
+        //*
+        //**
+        //***
+        //****
+        //*****
+
+        //com break
+        for (int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            for (int contadorColunas = 0; contadorColunas < 10; contadorColunas++)
+            {
+                Console.Write("*");
+                if (contadorColunas >= contadorLinhas)
+                    break;
+
+            }
+            Console.WriteLine();
+        }
+
+        //sem break
+        for (int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            for (int contadorColunas = 0; contadorColunas <= contadorLinhas; contadorColunas++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
