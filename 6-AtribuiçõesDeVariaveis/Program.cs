f68af9905bce027532fefeb29964ee07ef1baddd
Huel <39540176+HuelFra@users.noreply.github.com>
2022-11-07 21:23:54 -0300

    AtribuiçõesDeVariaveis
    Atribuindo valores para uma variável

---
+++ 6-AtribuiçõesDeVariaveis/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; //O bloco using provê ao desenvolvedor a habilidade de se criar um bloco de código
+              //isolado dentro de um determinado programa.
+
+class Programa
+{
+    static void Main(string[] args) 
+    {
+        Console.WriteLine("Executando projeto 6 - Atribuição de Variáveis");
+        int idade = 30;
+        int idadeAna = idade;
+
+        Console.WriteLine(idadeAna);
+
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();//Lê o que deve ser feito, no caso,não tem nada para fazer.                      
+    }
+}
