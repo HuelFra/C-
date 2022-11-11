56838cf6e65dc81c5880a66729a74fdad3321377
Huel <39540176+HuelFra@users.noreply.github.com>
2022-11-11 06:44:42 -0300

    ContaCorrente.cs
    Aprendendo sobre criação de módulos em C#

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+
+        public void Depositar(double valor) //criado o objeto de depósito = novo comportamento da classe ContaCorrente, método Depositar na conta corrente
+        {
+            this.saldo += valor; //this se refere ao próprio objeto criado
+        }
+    }
+}
