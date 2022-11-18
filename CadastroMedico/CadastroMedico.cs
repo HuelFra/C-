ef127c19bfc312cd6df20038b16e52711d18a80a
Huel <39540176+HuelFra@users.noreply.github.com>
2022-11-17 22:52:23 -0300

    CadastroMedico.cs
    Cadastro de nome, idade, altura, peso e IMC de uma pessoa.

---
+++ CadastroMedico/CadastroMedico.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroMedico
+{
+    public class CadastrosMedico
+    {
+        public string nome;
+        public int idade;
+        public double altura;
+        public double peso;
+        public double IMC;
+    }
+}
