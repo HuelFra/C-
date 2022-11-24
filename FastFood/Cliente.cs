2c73ca49f395694913f5bbe5d1157797922b7ff0
Huel <39540176+HuelFra@users.noreply.github.com>
2022-11-23 23:13:42 -0300

    Cliente.cs
    Classe - Cadastro de clientes. Serviço de FastFood.

---
+++ FastFood/Cliente.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FastFood.Pedido;
+
+namespace FastFood.Cliente
+{
+    public class Clientes
+    {
+        public int idcliente;
+        public string situacaocliente;
+        public string nome;
+        public string sexo;
+        public string cpf;
+        public string telefone;
+        public string celular;
+        public string email;
+        public string cidade;
+        public string rua;
+        public string numero;
+        public string complemento;
+        public string bairro;
+        public string cep;
+
+        public Clientes numpedido;
+
+    }
+}
