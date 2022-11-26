670882f6a09fc0b5a2a0f5042fee7a511b484293
Huel <39540176+HuelFra@users.noreply.github.com>
2022-11-26 17:55:18 -0300

    Veiculo.cs
    Usando herança

---
+++ Heranca/Veiculo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    public class Veiculo
+    {
+        public string marca = "Ford";
+        public void buzinar()
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+}
