c70e5d6d3a01ec74f780ffc4175ce047ff31fc5f
Huel <39540176+HuelFra@users.noreply.github.com>
2022-11-26 17:54:51 -0300

    Carro.cs
    Usando a herança

---
+++ Heranca/Carro.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    public class Carro : Veiculo
+    {
+        public string modeloNome = "Mustang";
+    }
+}
